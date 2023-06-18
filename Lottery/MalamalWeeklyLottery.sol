7101f4adf2149f9ef9fb255041f28178fa080160
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-18 17:21:29 +0545

    Update MalamalWeeklyLottery.sol

--- Lottery/MalamalWeeklyLottery.sol
+++ Lottery/MalamalWeeklyLottery.sol
@@ -5,6 +5,7 @@ contract malamalWeeklyLottery{
 
     address manager;
     address payable[] public participants;
+    uint lotteryBuyingDeadline = block.timestamp + 604800; // Valid only for 7 days, after than winner will choosen
 
     constructor(){
         manager = msg.sender;
@@ -20,11 +21,13 @@ contract malamalWeeklyLottery{
     }
 
     receive() external payable {
+        require(block.timestamp < lotteryBuyingDeadline,"Deadline has finished");
         require(msg.value == 1 ether,"Malamal Weekly Lottery Price is 1 ether");
         participants.push(payable(msg.sender));
     }
 
     function chooseRandomWinner() public payable onlyManager{
+        require(block.timestamp > lotteryBuyingDeadline,"Manager you must wait for atleast 7 days before choosing winner");
         require(participants.length > 1,"Participants must greater than 1");
         // For generating randomness you must use Oracals, this is only for test example
         uint random = uint(keccak256(abi.encodePacked(block.timestamp,participants.length)));
